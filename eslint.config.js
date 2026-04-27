44336ce49454acf44740ee55834e12dd28bf1c49
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-27 14:05:44 -0300

    Instalando e configurando o eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {},
+  }
+);
