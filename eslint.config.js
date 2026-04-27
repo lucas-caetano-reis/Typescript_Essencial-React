fef9e962982d7ec4c75dca7bf0f03f69b73e5813
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-27 14:50:44 -0300

    Instalando e configurando o Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ["dist", "node_modules"],
+    ignores: ['dist', 'node_modules'],
   },
 
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintConfigPrettier,
 
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
     rules: {},
-  }
+  },
 );
