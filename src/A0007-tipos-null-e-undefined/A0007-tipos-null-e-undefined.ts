4ca863fc3ded15bcae17a14ef7b73fad352768f2
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-28 12:56:06 -0300

    Tipos null e undefined

---
+++ src/A0007-tipos-null-e-undefined/A0007-tipos-null-e-undefined.ts
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string | undefined;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squareOf(x: any): number | null {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoString = squareOf('2');
+
+if (squareOfTwoString === null) {
+  console.log('Conta inválida');
+} else {
+  console.log(squareOfTwoString * 100);
+}
